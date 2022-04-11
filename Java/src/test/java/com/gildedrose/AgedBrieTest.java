acc78734e192b25557b4c8e8c1accafd1c52da42
bbouko <bastien.bouko@minfin.fed.be>
2022-04-11 23:56:31 +0200

    tests : add AgedBrieTest

---
+++ Java/src/test/java/com/gildedrose/AgedBrieTest.java
@@ -0,0 +1,35 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AgedBrieTest {
+
+    @Test
+    public void increasesInQuality(){
+        GildedRose app = new GildedRose(new Item("Aged Brie", 2, 0));
+        app.updateQuality();
+        assertEquals(app.items[0].quality, 1);
+        assertEquals(app.items[0].sellIn, 1);
+    }
+
+    @Test
+    void qualityNeverOver50(){
+        GildedRose app = new GildedRose(new Item("Aged Brie", 5, 50));
+        app.updateQuality();
+        assertEquals(app.items[0].quality, 50);
+        assertEquals(app.items[0].sellIn, 4);
+    }
+
+    @Test
+    void qualityIncreaseTwiceWhenSellByDateHasPassed(){
+        GildedRose app = new GildedRose(new Item("Aged Brie", 0, 2));
+        app.updateQuality();
+        assertEquals(app.items[0].quality, 4);
+        assertEquals(app.items[0].sellIn, -1);
+    }
+
+
+
+}
