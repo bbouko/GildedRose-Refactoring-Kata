4df9d7fa5110181ee4fce954f19a6a03d23b7550
bbouko <bastien.bouko@minfin.fed.be>
2022-04-12 08:18:59 +0200

    extract some methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,13 @@
 package com.gildedrose;
 
+
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String LEGENDARY = "Sulfuras, Hand of Ragnaros";
+
+    public static final int MIN_QUALITY = 0;
+    public static final int MAX_QUALITY = 50;
     Item[] items;
 
     public GildedRose(Item ... items) {
@@ -9,51 +16,69 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            updateItemQuality(item);
+        }
+    }
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+    private void updateItemQuality(Item item) {
+        updateQuality(item);
+        updateSellIn(item);
+        updateExpired(item);
+    }
+
+    private void updateExpired(Item item) {
+        if (isExpired(item)) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.quality > MIN_QUALITY) {
+                        if (!item.name.equals(LEGENDARY)) {
+                            item.quality = item.quality - 1;
                         }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    item.quality = MIN_QUALITY;
+                }
+            } else {
+                if (item.quality < MAX_QUALITY) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isExpired(Item item) {
+        return item.sellIn < MIN_QUALITY;
+    }
+
+    private void updateSellIn(Item item) {
+        if (!item.name.equals(LEGENDARY)) {
+            item.sellIn = item.sellIn - 1;
+
+        }
+    }
+
+    private void updateQuality(Item item) {
+        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.quality > MIN_QUALITY) {
+                if (!item.name.equals(LEGENDARY)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1;
+
+                if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < MAX_QUALITY) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < MAX_QUALITY) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
